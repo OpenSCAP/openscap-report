c42fd0559854e89a1faf506f449e5d75017652a1
Jan Rodak <hony.com@seznam.cz>
2021-08-12 10:47:22 +0200

    Create XSLT transformation

--- oscap_report/old_html_report_style/report_generator.py
+++ oscap_report/old_html_report_style/report_generator.py
@@ -1,7 +1,26 @@
+from io import BytesIO
+from pathlib import Path
+
+from lxml import etree
+
+XSL_DIR = Path(__file__).parent / "xsl"
+
 
 class ReportGenerator():
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, parser):
+        self.xml_report = parser.root
+        self.xslt_doc = etree.parse(str(XSL_DIR / "xccdf-report.xsl"))
+        self.xslt_transformer = etree.XSLT(self.xslt_doc)
 
     def generate_html_report(self):
-        return "<html><h1>TEST REPORT</h1><h2>{}</h2></html>\n".format(self.data)
+        html_report = self.xslt_transformer(self.xml_report)
+        result_html = etree.tostring(
+            html_report,
+            xml_declaration=True,
+            doctype="<!DOCTYPE html>",
+            encoding="utf-8",
+            standalone=False,
+            with_tail=False,
+            method="html",
+            pretty_print=False)
+        return BytesIO(result_html)
