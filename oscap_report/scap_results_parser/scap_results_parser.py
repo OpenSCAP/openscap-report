9fc9309d180987f2cdbe4348f660cf1fc2b5f35e
Jan Rodak <hony.com@seznam.cz>
2021-08-30 10:55:48 +0200

    Parse rules

--- oscap_report/scap_results_parser/scap_results_parser.py
+++ oscap_report/scap_results_parser/scap_results_parser.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from lxml import etree
 
-from .data_structures import Report
+from .data_structures import Remediation, Report, Rule
 from .namespaces import NAMESPACES
 
 SCHEMAS_DIR = Path(__file__).parent / "schemas"
@@ -69,5 +69,105 @@ class SCAPResultsParser():
             logging.debug(rule_id)
             logging.debug(rule)
 
+    def _insert_rules_results(self):
+        rules_results = self.test_results.findall('.//xccdf:rule-result', NAMESPACES)
+        for rule_result in rules_results:
+            rule_id = rule_result.get('idref')
+            self.rules[rule_id].time = rule_result.get('time')
+            self.rules[rule_id].result = rule_result.find('.//xccdf:result', NAMESPACES).text
+
+            check_content_ref = rule_result.find(".//xccdf:check-content-ref", NAMESPACES)
+            if check_content_ref is not None:
+                self.rules[rule_id].oval_definition_id = check_content_ref.get("name", "")
+
+            message = rule_result.find('.//xccdf:message', NAMESPACES)
+            if message is not None:
+                self.rules[rule_id].message = message.text
+
+    @staticmethod
+    def _get_references(rule):
+        references = []
+        for referenc in rule.findall(".//xccdf:reference", NAMESPACES):
+            ref = {}
+            ref["href"] = referenc.get("href")
+            ref["text"] = referenc.text
+            references.append(ref)
+        return references
+
+    @staticmethod
+    def _get_identifiers(rule):
+        identifiers = []
+        for identifier in rule.findall(".//xccdf:ident", NAMESPACES):
+            ident = {}
+            ident["system"] = identifier.get("system")
+            ident["text"] = identifier.text
+            identifiers.append(ident)
+        return identifiers
+
+    @staticmethod
+    def _get_warnings(rule):
+        warnings = []
+        for warning in rule.findall(".//xccdf:warning", NAMESPACES):
+            warnings.append(warning.text)
+        return warnings
+
+    @staticmethod
+    def _get_remediations(rule):
+        output = []
+        for fix in rule.findall(".//xccdf:fix", NAMESPACES):
+            fix_dict = {}
+            fix_dict["remediation_id"] = fix.get("id")
+            fix_dict["system"] = fix.get("system")
+            fix_dict["complexity"] = fix.get("complexity", "")
+            fix_dict["disruption"] = fix.get("disruption", "")
+            fix_dict["strategy"] = fix.get("strategy", "")
+            fix_dict["fix"] = fix.text
+            output.append(Remediation(**fix_dict))
+        return output
+
+    @staticmethod
+    def _get_multi_check(rule):
+        for check in rule.findall(".//xccdf:check", NAMESPACES):
+            if check.get("multi-check") == "true":
+                return True
+        return False
+
+    @staticmethod
+    def _get_full_description(rule):
+        description = rule.find(".//xccdf:description", NAMESPACES)
+        str_description = etree.tostring(description).decode()
+        start_tag_description = str_description.find(">") + 1
+        end_tag_description = str_description.rfind("</")
+        return str_description[start_tag_description:end_tag_description].replace("html:", "")
+
+    def get_info_about_rules_in_profile(self):
+        rules = {}
+        for rule in self.root.findall(".//xccdf:Rule", NAMESPACES):
+            rule_dict = {}
+            rule_id = rule.get("id")
+            rule_dict["rule_id"] = rule_id
+            rule_dict["severity"] = rule.get("severity")
+            rule_dict["title"] = rule.find(".//xccdf:title", NAMESPACES).text
+            rule_dict["description"] = self._get_full_description(rule)
+            rule_dict["references"] = self._get_references(rule)
+            rule_dict["rationale"] = rule.find(".//xccdf:rationale", NAMESPACES).text
+
+            platform = rule.find(".//xccdf:platform", NAMESPACES)
+            if platform is not None:
+                rule_dict["platform"] = platform.get("idref")
+
+            rule_dict["identifiers"] = self._get_identifiers(rule)
+            rule_dict["warnings"] = self._get_warnings(rule)
+            rule_dict["remediations"] = self._get_remediations(rule)
+            rule_dict["multi_check"] = self._get_multi_check(rule)
+            rules[rule_id] = Rule(**rule_dict)
+        return rules
+
     def parse_report(self):
-        return self.get_profile_info()
+        self.profile = self.get_profile_info()
+        logging.debug(self.profile)
+        self.rules = self.get_info_about_rules_in_profile()
+        self._insert_rules_results()
+        self._debug_show_rules()
+        self.profile.rules = self.rules
+        return self.profile
