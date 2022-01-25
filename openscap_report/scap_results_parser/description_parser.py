4b765d3030357be4a72d6bbfeb3b1ab67726e665
Jan Rodak <hony.com@seznam.cz>
2022-01-25 16:10:08 +0100

    Process XML tag sub in description and in remediation code

--- openscap_report/scap_results_parser/description_parser.py
+++ openscap_report/scap_results_parser/description_parser.py
@@ -4,12 +4,46 @@ from .namespaces import NAMESPACES
 
 
 class DescriptionParser():
+    def __init__(self, ref_values):
+        self.ref_values = ref_values
+
+    def replace_sub_tag(self, tag):
+        return self.ref_values.get(tag.get("idref"))
+
     @staticmethod
-    def get_full_description(item):
-        description = item.find(".//xccdf:description", NAMESPACES)
+    def get_html_attributes_as_string(attributes):
+        out = ""
+        for key, value in attributes.items():
+            out += f" {key}=\"{value}\""
+        return out
+
+    def get_html_tag_as_string(self, tag):
+        tag_name = etree.QName(tag).localname
+        tag_text = tag.text
+        tag_attributes = self.get_html_attributes_as_string(tag.attrib)
+        for child in tag:
+            if tag_text is None:
+                tag_text = ""
+            if child.prefix == "html":
+                tag_text += self.get_html_tag_as_string(child)
+            if etree.QName(child).localname == "sub":
+                tag_text += self.replace_sub_tag(child)
+            tag_text += child.tail if child.tail is not None else ""
+        if tag_text is not None:
+            return f"<{tag_name}{tag_attributes}>{tag_text}</{tag_name}>"
+        return f"<{tag_name}{tag_attributes}>"
+
+    def get_full_description(self, rule):
+        description = rule.find(".//xccdf:description", NAMESPACES)
         if description is None:
             return None
-        str_description = etree.tostring(description).decode()
-        start_tag_description = str_description.find(">") + 1
-        end_tag_description = str_description.rfind("</")
-        return str_description[start_tag_description:end_tag_description].replace("html:", "")
+        str_description = description.text
+        for child in description:
+            if str_description is None:
+                str_description = ""
+            if child.prefix == "html":
+                str_description += self.get_html_tag_as_string(child)
+            if etree.QName(child).localname == "sub":
+                str_description += self.replace_sub_tag(child)
+            str_description += child.tail if child.tail is not None else ""
+        return str_description
---
+++ openscap_report/scap_results_parser/remediation_parser.py
@@ -0,0 +1,32 @@
+from lxml import etree
+
+from .data_structures.data_structures import Remediation
+
+
+class RemediationParser():
+    def __init__(self, ref_values):
+        self.ref_values = ref_values
+
+    def replace_sub_tag(self, tag):
+        return self.ref_values.get(tag.get("idref"))
+
+    def _get_remediation_code(self, fix):
+        str_fix = fix.text
+        for child in fix:
+            if str_fix is None:
+                str_fix = ""
+            if etree.QName(child).localname == "sub":
+                str_fix += self.replace_sub_tag(child)
+            str_fix += child.tail if child.tail is not None else ""
+        return str_fix
+
+    def get_remediation(self, fix):
+        fix_dict = {
+            "remediation_id": fix.get("id"),
+            "system": fix.get("system"),
+            "complexity": fix.get("complexity", ""),
+            "disruption": fix.get("disruption", ""),
+            "strategy": fix.get("strategy", ""),
+            "fix": self._get_remediation_code(fix),
+        }
+        return Remediation(**fix_dict)
