d8d2b5d311d9c222823131e666dd7ea532fb86ce
Jan Rodak <hony.com@seznam.cz>
2022-04-22 15:24:37 +0200

    Add button to html report

--- openscap_report/html_report/templates/js/debug_script.js
+++ openscap_report/html_report/templates/js/debug_script.js
@@ -1,18 +1,62 @@
 
-function show_all_rules_details(self) { // eslint-disable-line no-unused-vars
-    self.setAttribute('disabled', 'disabled');
+function show_all_rules_details(self, expand_oval_test_details=false) { // eslint-disable-line no-unused-vars
+    var other_button = get_other_button(self);
+    if (other_button != undefined) {
+        other_button.disabled = true;
+    }
+    self.disabled = true;
     const promises = []
     const selector = "table[id=rule-table] tbody[rule-id] button[id=show_hide_rule_detail_button]";
     const rule_buttons = document.querySelectorAll(selector);
     rule_buttons.forEach(function (item) {
         promises.push(new Promise(resolve => {
             setTimeout(() => {
-            resolve(show_rule_detail(item)); // eslint-disable-line no-undef
+            resolve(show_rule_detail_and_expand_oval_test_details(item, expand_oval_test_details)); // eslint-disable-line no-undef
             }, 0.001);
         }))
     });
     Promise.all(promises).then(() => {
-        self.textContent = self.textContent == 'Show all result details' ? 'Hide all result details' : 'Show all result details';
-        self.removeAttribute('disabled');
+        enable_and_change_text_of_button(self);
+        if (other_button != undefined) {
+            enable_and_change_text_of_button(other_button);
+        }
     });
+}
+
+function enable_and_change_text_of_button(button) {
+    if (button == undefined) {
+        return;
+    }
+    button.disabled = false;
+    if (button.textContent.includes("Show")) {
+        button.textContent = button.textContent.replace("Show", "Hide");
+    } else {
+        button.textContent = button.textContent.replace("Hide", "Show");
+    }
+}
+
+
+function show_rule_detail_and_expand_oval_test_details(item, expand_oval_test_details) {
+    show_rule_detail(item); // eslint-disable-line no-undef
+    if (expand_oval_test_details) {
+        click_on_oval_test_details(item.parentNode.parentNode.parentNode);
+    }
+}
+
+
+function click_on_oval_test_details(expanded_rule) {
+    var selector = 'div[id^="info_of_test_"]';
+    expanded_rule.querySelectorAll(selector).forEach(function (div) {
+        if (div.style.display === "none") {
+            var button = div.previousSibling;
+            button.click();
+        }
+    });
+}
+
+function get_other_button(button) {
+    if (button.getAttribute("id") == "show-all-rules-details") {
+        return button.parentNode.parentNode.querySelector(`button[id=show-all-oval-test-details]`);
+    }
+    return button.parentNode.parentNode.querySelector(`button[id=show-all-rules-details]`);
 }
