1ff4f6e970e31c0fc4eb3215f7d35971e8439921
Jan Rodak <hony.com@seznam.cz>
2022-06-07 14:07:07 +0200

    Load version from installed package

--- openscap_report/__init__.py
+++ openscap_report/__init__.py
@@ -1,4 +1,14 @@
 # Copyright 2022, Red Hat, Inc.
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
-__version__ = '0.1.1'
+from os import path
+
+from pkg_resources import DistributionNotFound, get_distribution
+
+DISTRIBUTION_NAME = "openscap-report"
+try:
+    distribution = get_distribution(DISTRIBUTION_NAME)
+except DistributionNotFound:
+    __version__ = f"Version is unavailable. Please install {DISTRIBUTION_NAME}!"
+else:
+    __version__ = distribution.version
