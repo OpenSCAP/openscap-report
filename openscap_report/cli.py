154e22c58c0f91afe99a2985ab5a4e7f5cff1b19
Jan Rodak <hony.com@seznam.cz>
2022-06-16 16:37:10 +0200

    Remove if statments

--- openscap_report/cli.py
+++ openscap_report/cli.py
@@ -176,18 +176,32 @@ class DebugSetting():
     use_online_css: bool = False
     button_show_all_rules_and_oval_test_details: bool = False
 
+    def set_no_minify(self, val):
+        self.no_minify = val
+
+    def set_button_show_all_rules(self, val):
+        self.button_show_all_rules = val
+
+    def set_use_online_css(self, val):
+        self.use_online_css = val
+
+    def set_button_show_all_rules_and_oval_test_details(self, val):
+        self.button_show_all_rules_and_oval_test_details = val
+
     def update_settings_with_debug_flags(self, debug_flags):
+        flags = {
+            "NO-MINIFY": self.set_no_minify,
+            "BUTTON-SHOW-ALL-RULES": self.set_button_show_all_rules,
+            "ONLINE-CSS": self.set_use_online_css,
+            "BUTTON-SHOW-ALL-RULES-AND-OVAL-TEST-DETAILS":
+                self.set_button_show_all_rules_and_oval_test_details,
+        }
+
         for flag in debug_flags:
             if flag in self.options_require_debug_script:
                 self.include_debug_script = True
-            if flag == "NO-MINIFY":
-                self.no_minify = True
-            if flag == "BUTTON-SHOW-ALL-RULES":
-                self.button_show_all_rules = True
-            if flag == "ONLINE-CSS":
-                self.use_online_css = True
-            if flag == "BUTTON-SHOW-ALL-RULES-AND-OVAL-TEST-DETAILS":
-                self.button_show_all_rules_and_oval_test_details = True
+            if flag in flags:
+                flags[flag](True)
 
 
 def main():
