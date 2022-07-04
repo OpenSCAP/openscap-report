7ab6c2138d66c643d0a9df7bc294e03d6ca8c50d
Jan Rodak <hony.com@seznam.cz>
2022-07-04 14:39:43 +0200

    Use buildin function asdict

--- openscap_report/scap_results_parser/data_structures/group.py
+++ openscap_report/scap_results_parser/data_structures/group.py
@@ -1,7 +1,7 @@
 # Copyright 2022, Red Hat, Inc.
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass
 
 
 @dataclass
@@ -14,20 +14,4 @@ class Group:
     sub_groups: list = None
 
     def as_dict(self):
-        if not self.sub_groups:
-            return {
-                "group_id": self.group_id,
-                "title": self.title,
-                "description": self.description,
-                "platforms": self.platforms,
-                "rules_ids": self.rules_ids,
-                "sub_groups": None,
-            }
-        return {
-            "group_id": self.group_id,
-            "title": self.title,
-            "description": self.description,
-            "platforms": self.platforms,
-            "rules_ids": self.rules_ids,
-            "sub_groups": [sub_group.as_dict() for sub_group in self.sub_groups],
-        }
+        return asdict(self)
