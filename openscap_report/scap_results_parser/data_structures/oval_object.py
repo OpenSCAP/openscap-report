75613651c8d34c608c745220f93f0bd039953d75
Jan Rodak <hony.com@seznam.cz>
2022-07-07 10:59:49 +0200

    Specify data types of OvalObject

--- openscap_report/scap_results_parser/data_structures/oval_object.py
+++ openscap_report/scap_results_parser/data_structures/oval_object.py
@@ -1,15 +1,15 @@
 # Copyright 2022, Red Hat, Inc.
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
-from dataclasses import asdict, dataclass
+from dataclasses import asdict, dataclass, field
 
 
 @dataclass
-class OvalObject():
-    object_id: str = ""
+class OvalObject:
+    object_id: str
     flag: str = ""
     object_type: str = ""
-    object_data: dict = None
+    object_data: list[dict[str, str]] = field(default_factory=list)
 
     def as_dict(self):
         return asdict(self)
