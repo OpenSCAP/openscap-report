a100c85bcfbf5933924acc697e377f52f59a998a
Jan Rodak <hony.com@seznam.cz>
2022-09-02 14:31:16 +0200

    Create OVAL definition

--- openscap_report/scap_results_parser/data_structures/__init__.py
+++ openscap_report/scap_results_parser/data_structures/__init__.py
@@ -3,6 +3,7 @@
 
 from .group import Group
 from .identifier import Identifier
+from .oval_definition import OvalDefinition
 from .oval_node import OvalNode
 from .oval_object import OvalObject
 from .oval_result_eval import (EMPTY_RESULT, FULL_RESULT_TO_SHORT_RESULT,
