b546d50dd409fbf010cac5d91bd4733702f0025f
Jan Rodak <hony.com@seznam.cz>
2022-10-03 17:10:32 +0200

    Add parser of performed scan information

--- openscap_report/scap_results_parser/parsers/__init__.py
+++ openscap_report/scap_results_parser/parsers/__init__.py
@@ -6,6 +6,8 @@ from .group_parser import GroupParser
 from .oval_definition_parser import OVALDefinitionParser
 from .oval_result_parser import OVALResultParser
 from .oval_test_info_parser import OVALTestInfoParser
+from .profile_info_parser import ProfileInfoParser
 from .remediation_parser import RemediationParser
 from .report_parser import ReportParser
 from .rule_parser import RuleParser
+from .scan_result_parser import ScanResultParser
