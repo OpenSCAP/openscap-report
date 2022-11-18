9cfb846c10594c05e7773398a972f14d4dd046a7
Jan Rodak <hony.com@seznam.cz>
2022-11-18 17:36:12 +0100

    Create cpe platform

---
+++ openscap_report/scap_results_parser/data_structures/cpe_platform.py
@@ -0,0 +1,17 @@
+# Copyright 2022, Red Hat, Inc.
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+from dataclasses import asdict, dataclass
+
+from .cpe_logical_test import LogicalTest
+
+
+@dataclass
+class Platform:
+    platform_id: str
+    logical_test: LogicalTest
+    title: str = ""
+    result: str = ""
+
+    def as_dict(self):
+        return asdict(self)
