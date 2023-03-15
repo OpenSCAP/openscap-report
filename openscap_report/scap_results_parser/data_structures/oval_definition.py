f850d768681e6cd3f0a8dba010ea8164f0217e23
Jan Rodak <hony.com@seznam.cz>
2023-03-15 11:22:39 +0100

    Add explanation of result of OVAL definition

--- openscap_report/scap_results_parser/data_structures/oval_definition.py
+++ openscap_report/scap_results_parser/data_structures/oval_definition.py
@@ -1,4 +1,3 @@
-
 from dataclasses import asdict, dataclass, field
 from typing import List
 
@@ -11,6 +10,29 @@ OVAL_DEFINITION_JSON_KEYS = [
     "description",
     "version",
 ]
+
+OVAL_CLASS_DESCRIPTION = {
+    "compliance": (
+        "Compliance class describes OVAL Definitions that check to see if a system's state is "
+        'compliant with a specific policy. An evaluation result of "true", for this class of OVAL '
+        "Definitions, indicates that a system is compliant with the stated policy."
+    ),
+    "vulnerability": (
+        "Vulnerability class describes OVAL Definitions that check to see if the system is in a "
+        'vulnerable state. An evaluation result of "true", for this class of OVAL Definitions, '
+        "indicates that the system is in a vulnerable state."
+    ),
+    "inventory": (
+        "Inventory class describes OVAL Definitions that check to see if a piece of software is "
+        'installed on a system. An evaluation result of "true", for this class of OVAL '
+        "Definitions, indicates that the specified software is installed on the system."
+    ),
+    "patch": (
+        "Patch class describes OVAL Definitions that check to see if a patch should be installed "
+        'on a system. An evaluation result of "true", for this class of OVAL Definitions, '
+        "indicates that the specified patch should be installed on the system."
+    ),
+}
 
 
 @dataclass
@@ -25,3 +47,6 @@ class OvalDefinition:
 
     def as_dict(self):
         return asdict(self)
+
+    def get_oval_class_description(self):
+        return OVAL_CLASS_DESCRIPTION[self.definition_class]
