ead13024ab40a153fdca743a4d313118a952d170
Jan Rodak <hony.com@seznam.cz>
2023-06-19 17:27:07 +0200

    Add explanations

--- openscap_report/report_generators/html_templates/js/oval_graph_generation_script.js
+++ openscap_report/report_generators/html_templates/js/oval_graph_generation_script.js
@@ -60,6 +60,17 @@ const COLOR_TRANSLATION = {
     'pf-m-red': '--pf-global--danger-color--200',
     '': ''
 };
+const OVAL_OPERATOR_EXPLANATION = {
+    'AND': 'The AND operator produces a true result if every argument is true. If one or more arguments are false, the result of the AND is false. If one or more of the arguments are unknown, and if none of the arguments are false, then the AND operator produces a result of unknown.',
+    'OR': 'The OR operator produces a true result if one or more arguments is true. If every argument is false, the result of the OR is false. If one or more of the arguments are unknown and if none of arguments are true, then the OR operator produces a result of unknown.',
+    'XOR': 'XOR is defined to be true if an odd number of its arguments are true, and false otherwise. If any of the arguments are unknown, then the XOR operator produces a result of unknown.',
+    'ONE': 'The ONE operator produces a true result if one and only one argument is true. If there are more than argument is true (or if there are no true arguments), the result of the ONE is false. If one or more of the arguments are unknown, then the ONE operator produces a result of unknown.',
+    'NOT': 'NOT is negation, then the true result is false and the false result is true, all other results remain the same.'
+};
+const CPE_AL_OPERATOR_EXPLANATION = {
+    'AND': 'The AND operator produces a true result if every argument is true. If one or more arguments are false, the result of the AND is false.',
+    'OR': 'The OR operator produces a true result if one or more arguments is true. If every argument is false, the result of the OR is false.'
+};
 
 const DIV = document.createElement("div");
 const SPAN = document.createElement("span");
