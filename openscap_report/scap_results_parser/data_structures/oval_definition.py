a94cb99d839a05ff344186fb7d19471a43323c79
Jan Černý <jcerny@redhat.com>
2024-07-12 16:59:02 +0200

    Remove OVAL definition class information
    In normal SCAP content, the OVAL definitions have always
    class attribute set to compliance. It isn't necessary to show
    this information in every rule in HTML report and add a long
    description, it only creates noise.

--- openscap_report/scap_results_parser/data_structures/oval_definition.py
+++ openscap_report/scap_results_parser/data_structures/oval_definition.py
@@ -15,34 +15,10 @@ OVAL_DEFINITION_JSON_KEYS = [
     "version",
 ]
 
-OVAL_CLASS_DESCRIPTION = {
-    "compliance": (
-        "Compliance class describes OVAL Definitions that check to see if a system's state is "
-        'compliant with a specific policy. An evaluation result of "true", for this class of OVAL '
-        "Definitions, indicates that a system is compliant with the stated policy."
-    ),
-    "vulnerability": (
-        "Vulnerability class describes OVAL Definitions that check to see if the system is in a "
-        'vulnerable state. An evaluation result of "true", for this class of OVAL Definitions, '
-        "indicates that the system is in a vulnerable state."
-    ),
-    "inventory": (
-        "Inventory class describes OVAL Definitions that check to see if a piece of software is "
-        'installed on a system. An evaluation result of "true", for this class of OVAL '
-        "Definitions, indicates that the specified software is installed on the system."
-    ),
-    "patch": (
-        "Patch class describes OVAL Definitions that check to see if a patch should be installed "
-        'on a system. An evaluation result of "true", for this class of OVAL Definitions, '
-        "indicates that the specified patch should be installed on the system."
-    ),
-}
-
 
 @dataclass
 class OvalDefinition:
     definition_id: str
-    definition_class: str
     title: str
     description: str = ""
     version: str = ""
@@ -51,6 +27,3 @@ class OvalDefinition:
 
     def as_dict(self):
         return asdict(self)
-
-    def get_oval_class_description(self):
-        return OVAL_CLASS_DESCRIPTION[self.definition_class]
